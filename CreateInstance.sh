b208a6d65dc80f400fd0263860a1320b6574ba1c
okachurin <oleksandr_kachurin@epam.com>
2018-01-09 11:52:54 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -1,1 +1,6 @@
-/var/lib/jenkins/.local/bin/aws ec2 run-instances --image-id ami-df8406b0 --count 1 --instance-type $TYPE --key-name WINDOWS-SERVER --security-groups launch-wizard-2 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SLAVE},{Key=APPLICATION,Value=Tomcat}]' 
+#!/bin/bash
+/var/lib/jenkins/.local/bin/aws ec2 run-instances --image-id ami-df8406b0 
+--count 1
+--instance-type $TYPE --key-name WINDOWS-SERVER 
+--security-groups launch-wizard-2 
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SLAVE},{Key=APPLICATION,Value=Tomcat}]' 
