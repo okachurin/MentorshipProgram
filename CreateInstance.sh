7049bf379cae21f765d5949722f5077c5f21bca6
okachurin <oleksandr_kachurin@epam.com>
2018-01-17 16:20:19 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws ec2 run-instances --image-id ami-df8406b0 \
  --count 1 \
- --instance-type $TYPE --key-name WINDOWS-SERVER \ 
+ --instance-type $INSTANCETYPE --key-name WINDOWS-SERVER \ 
  --security-groups launch-wizard-2 \
  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SLAVE},{Key=APPLICATION,Value=Tomcat}]' 
