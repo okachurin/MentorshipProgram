b3131a0b8568a9d9aed41642df69659f4aca0c8e
okachurin <oleksandr_kachurin@epam.com>
2018-01-17 11:13:38 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws ec2 run-instances --image-id $INSTANCE-AMI \
+aws ec2 run-instances --image-id $INSTANCEAMI \
 --count 1 \
---instance-type $INSTANCE-TYPE --key-name $INSTANCE-KEY \ 
---security-groups $SECURITY-GROUP \
+--instance-type $INSTANCE-TYPE --key-name $INSTANCEKEY \ 
+--security-groups $SECURITYGROUP \
 --tag-specifications 'ResourceType=instance,Tags=[{$TAG1},{$TAG2}]' 
