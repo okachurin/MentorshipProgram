8c851940b2d951477fe94b01ca2cc819a6388b43
okachurin <oleksandr_kachurin@epam.com>
2018-01-17 16:25:48 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-df8406b0 
---count 1 
---instance-type $INSTANCETYPE --key-name WINDOWS-SERVER 
---security-group-ids $SECURITYGROUP 
+aws ec2 run-instances --image-id ami-df8406b0 \
+--count 1 \
+--instance-type $INSTANCETYPE --key-name WINDOWS-SERVER \
+--security-group-ids $SECURITYGROUP \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SLAVE},{Key=APPLICATION,Value=Tomcat}]' 
