5e0a26a3ed49544009f60935960c1268e182c450
okachurin <oleksandr_kachurin@epam.com>
2018-01-18 13:42:41 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -3,4 +3,8 @@ aws ec2 run-instances --image-id $INSTANCEAMI \
 --count 1 \
 --instance-type $INSTANCETYPE --key-name $INSTANCEKEY \
 --security-group-ids $SECURITYGROUP \
---tag-specifications 'ResourceType=instance,Tags=[{Key="APPLICATION",Value="TOMCAT"}]' 
+--tag-specifications 'ResourceType=instance,Tags=[{Key="APPLICATION",Value="TOMCAT"}]' |
+while [ "aws ec2 describe-instances --filters "Name=tag-key,Values=APPLICATION" "Name=instance-state-code,Values=16"" ]
+do
+echo "Instance up and Running"
+done
