0deac51612880ed4b851f37ffb95fe5c760049ea
okachurin <oleksandr_kachurin@epam.com>
2018-02-06 11:01:12 +0200

    Update CreateInstance.sh

--- CreateInstance.sh
+++ CreateInstance.sh
@@ -4,6 +4,7 @@ aws ec2 run-instances --image-id $INSTANCEAMI \
 --instance-type $INSTANCETYPE --key-name $INSTANCEKEY \
 --security-group-ids $SECURITYGROUP \
 --tag-specifications 'ResourceType=instance,Tags=[{Key="APPLICATION",Value="TOMCAT"}]' |
+--user-data file://ud.txt
 while [ ! "aws ec2 describe-instances --filters "Name=tag-key,Values=APPLICATION" "Name=instance-state-code,Values=16"" ]
 do
 echo "Instance PENDING"
