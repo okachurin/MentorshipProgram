48716413bded019cb24578cdfec63b1ef5c0ba08
okachurin <oleksandr_kachurin@epam.com>
2018-03-01 12:21:18 +0200

    Create CreateSSH.sh

---
+++ CreateSSH.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo scp -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/WINDOWS-SERVER.pem /var/lib/jenkins/.ssh/id_rsa.pub ubuntu@$IP:
+sudo ssh -o StrictHostKeyChecking=no -i  /home/ubuntu/.ssh/WINDOWS-SERVER.pem ubuntu@$IP 'cat id_rsa.pub >> ~/.ssh/authorized_keys'
+ssh-keyscan $IP >> /var/lib/jenkins/.ssh/known_hosts
+
+sed -i 's/ipaddress/'$IP'/g' ansibleip.txt
