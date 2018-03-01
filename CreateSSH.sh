9198c1cffeea0aa9cdd6067a9495a3dea478080b
okachurin <oleksandr_kachurin@epam.com>
2018-03-01 12:22:22 +0200

    Update CreateSSH.sh

--- CreateSSH.sh
+++ CreateSSH.sh
@@ -3,4 +3,4 @@ sudo scp -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/WINDOWS-SERVER.pem /va
 sudo ssh -o StrictHostKeyChecking=no -i  /home/ubuntu/.ssh/WINDOWS-SERVER.pem ubuntu@$IP 'cat id_rsa.pub >> ~/.ssh/authorized_keys'
 ssh-keyscan $IP >> /var/lib/jenkins/.ssh/known_hosts
 
-sed -i 's/ipaddress/'$IP'/g' ansibleip.txt
+sed -i 's/ansibleip/'$IP'/g' instanceip.txt
